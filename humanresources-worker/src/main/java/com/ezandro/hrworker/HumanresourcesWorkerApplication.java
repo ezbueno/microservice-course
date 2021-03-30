8842f85a8f08fb9643a8365b8288fe2bd67fcbf8
Ezandro Bueno <ezandrobueno@yahoo.com.br>
2021-03-30 17:30:29 -0300

    Project humanresources-worker created

---
+++ humanresources-worker/src/main/java/com/ezandro/hrworker/HumanresourcesWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.ezandro.hrworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanresourcesWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanresourcesWorkerApplication.class, args);
+	}
+
+}
