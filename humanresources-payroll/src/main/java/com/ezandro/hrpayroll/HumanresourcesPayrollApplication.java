18e6317bcdda9e32dd7a323134cd74e03413e316
Ezandro Bueno <ezandrobueno@yahoo.com.br>
2021-03-30 18:32:37 -0300

    Project humanresources-payroll created

---
+++ humanresources-payroll/src/main/java/com/ezandro/hrpayroll/HumanresourcesPayrollApplication.java
@@ -0,0 +1,13 @@
+package com.ezandro.hrpayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanresourcesPayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanresourcesPayrollApplication.class, args);
+	}
+
+}
---
+++ humanresources-payroll/src/test/java/com/ezandro/hrpayroll/HumanresourcesPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezandro.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanresourcesPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
