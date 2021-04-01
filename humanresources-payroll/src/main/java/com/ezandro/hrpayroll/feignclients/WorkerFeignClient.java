8b9c5c84e34454887803e8dd23ee2b99cea4b127
Ezandro Bueno <ezandrobueno@yahoo.com.br>
2021-04-01 14:43:59 -0300

    Feign Client

---
+++ humanresources-payroll/src/main/java/com/ezandro/hrpayroll/feignclients/WorkerFeignClient.java
@@ -0,0 +1,18 @@
+package com.ezandro.hrpayroll.feignclients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.ezandro.hrpayroll.entities.Worker;
+
+@Component
+@FeignClient(name = "humanresources-worker", url = "localhost:8001", path = "/workers")
+public interface WorkerFeignClient {
+	
+	@GetMapping(value = "/{id}")
+	ResponseEntity<Worker> findById(@PathVariable Long id);
+	
+}
