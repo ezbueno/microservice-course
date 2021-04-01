c801b46ee736fdb2da036d18dbccbd0499661a28
Ezandro Bueno <ezandrobueno@yahoo.com.br>
2021-04-01 16:51:26 -0300

    Project humanresources-eureka-server created

---
+++ humanresources-eureka-server/src/main/java/com/ezandro/hreurekaserver/HumanresourcesEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.ezandro.hreurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanresourcesEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanresourcesEurekaServerApplication.class, args);
+	}
+
+}
---
+++ humanresources-eureka-server/src/test/java/com/ezandro/hreurekaserver/HumanresourcesEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezandro.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanresourcesEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
