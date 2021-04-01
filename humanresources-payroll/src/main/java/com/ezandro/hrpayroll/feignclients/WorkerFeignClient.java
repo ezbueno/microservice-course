f6cba89ad5661a3fd102912ce398fe55fddb048b
Ezandro Bueno <ezandrobueno@yahoo.com.br>
2021-04-01 16:24:53 -0300

    Ribbon load balancing

--- humanresources-payroll/src/main/java/com/ezandro/hrpayroll/feignclients/WorkerFeignClient.java
+++ humanresources-payroll/src/main/java/com/ezandro/hrpayroll/feignclients/WorkerFeignClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.ezandro.hrpayroll.entities.Worker;
 
 @Component
-@FeignClient(name = "humanresources-worker", url = "localhost:8001", path = "/workers")
+@FeignClient(name = "humanresources-worker", path = "/workers")
 public interface WorkerFeignClient {
 	
 	@GetMapping(value = "/{id}")
