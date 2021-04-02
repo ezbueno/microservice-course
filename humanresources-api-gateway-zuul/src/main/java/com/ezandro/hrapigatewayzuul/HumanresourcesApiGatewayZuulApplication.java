f6469e47a3db975d04ef7d3ad09ff6fc35a4f68d
Ezandro Bueno <ezandrobueno@yahoo.com.br>
2021-04-02 14:05:46 -0300

    Project humanresources-api-gateway-zuul created

---
+++ humanresources-api-gateway-zuul/src/main/java/com/ezandro/hrapigatewayzuul/HumanresourcesApiGatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package com.ezandro.hrapigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanresourcesApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanresourcesApiGatewayZuulApplication.class, args);
+	}
+
+}
