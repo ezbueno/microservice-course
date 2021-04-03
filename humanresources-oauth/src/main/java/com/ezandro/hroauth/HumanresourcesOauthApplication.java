4b9a688fd4542632c7c06bd99b9be8e5b87457b9
Ezandro Bueno <ezandrobueno@yahoo.com.br>
2021-04-03 14:23:41 -0300

    Project humanresources-oauth created

---
+++ humanresources-oauth/src/main/java/com/ezandro/hroauth/HumanresourcesOauthApplication.java
@@ -0,0 +1,13 @@
+package com.ezandro.hroauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanresourcesOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanresourcesOauthApplication.class, args);
+	}
+
+}
---
+++ humanresources-oauth/src/test/java/com/ezandro/hroauth/HumanresourcesOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezandro.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanresourcesOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
