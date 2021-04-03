95c804183b36bb84e404fa42751239a8720510eb
Ezandro Bueno <ezandrobueno@yahoo.com.br>
2021-04-03 13:11:04 -0300

    Project humanresources-user created

---
+++ humanresources-user/src/main/java/com/ezandro/hruser/HumanresourcesUserApplication.java
@@ -0,0 +1,13 @@
+package com.ezandro.hruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanresourcesUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanresourcesUserApplication.class, args);
+	}
+
+}
---
+++ humanresources-user/src/test/java/com/ezandro/hruser/HumanresourcesUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezandro.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanresourcesUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
